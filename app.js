7f1a6a43431bb8819b5bee1ddccfa615a1b3b759
itagaki <itagaki.shintaro@gmail.com>
2016-01-01 12:11:00 +0900

    Add menu for copy & paste
    http://yonda1229.hatenablog.com/entry/2015/10/03/163300

--- app.js
+++ app.js
@@ -40,4 +40,81 @@ app.on( 'ready', function () {
         // when you should delete the corresponding element.
         mainWindow = null;
     } );
+
+    // Add menu
+    setApplicationMenu();
 } );
+
+
+// http://yonda1229.hatenablog.com/entry/2015/10/03/163300
+var setApplicationMenu = () => {
+    var Menu = require( 'menu' );
+    var menu = Menu.buildFromTemplate( [
+        {
+            label: "Application",
+            submenu: [
+                {
+                    label: "About Application",
+                    selector: "orderFrontStandardAboutPanel:"
+                },
+                {
+                    type: "separator"
+                },
+                {
+                    label: "Quit",
+                    accelerator: "Command+Q",
+                    click: function () {
+                        app.quit();
+                    }
+                }
+      ]
+    },
+        {
+            label: "View",
+            submenu: [
+                {
+                    label: 'Reload',
+                    accelerator: 'CmdOrCtrl+R',
+                    click: function ( item, focusedWindow ) {
+                        if ( focusedWindow )
+                            focusedWindow.reload();
+                    }
+        },
+      ]
+    },
+        {
+            label: "Edit",
+            submenu: [
+                {
+                    label: "Undo",
+                    accelerator: "Command+Z",
+                    selector: "undo:"
+                },
+                {
+                    label: "Redo",
+                    accelerator: "Shift+Command+Z",
+                    selector: "redo:"
+                },
+                {
+                    type: "separator"
+                },
+                {
+                    label: "Cut",
+                    accelerator: "Command+X",
+                    selector: "cut:"
+                },
+                {
+                    label: "Copy",
+                    accelerator: "Command+C",
+                    selector: "copy:"
+                },
+                {
+                    label: "Paste",
+                    accelerator: "Command+V",
+                    selector: "paste:"
+                }
+      ]
+    }
+  ] );
+    Menu.setApplicationMenu( menu );
+}
